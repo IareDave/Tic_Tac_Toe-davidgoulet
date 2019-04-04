acd359a1933d8a48c821023c5245d4ac442b577e
IareDave <gouletdavid95@gmail.com>
2019-04-04 17:34:20 -0400

    Create basic functionality
    Adjusted gameboard CSS, added functionality to add X or Os on board and
     button to reset board

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,7 +7,42 @@
 // require('./example')
 
 $(() => {
-  $('div').click(function () {
-    $('.box1').text('yo')
+  // Player and win condition setup
+  const gameLogic = {
+    gameBoard: 1,
+    playerOne: 'X',
+    playerTwo: 'O',
+    playerCounter: 0,
+    winCombos: [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
+  }
+
+  // Set HTML tictactoe boxes to variable
+  const cells = document.querySelectorAll('.tic-box')
+
+  // Click handler
+  $('.tic-box').on('click', function () {
+    if (cells !== ('X' || 'O')) {
+      if (gameLogic.playerCounter % 2 === 0) {
+        $(this).append(gameLogic.playerOne)
+        gameLogic.playerCounter++
+      } else {
+        $(this).append(gameLogic.playerTwo)
+        gameLogic.playerCounter++
+      }
+    }
+  })
+
+  // Clear game board
+  $('button').on('click', function () {
+    $('.tic-box').text('')
   })
 })
